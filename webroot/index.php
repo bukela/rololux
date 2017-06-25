be18e81d221727bdc39049dfa55ad4f10944813c
bukela <bukela@gmail.com>
2017-06-26 00:19:57 +0200

    footer

--- webroot/index.php
+++ webroot/index.php
@@ -3,25 +3,24 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="img/favicon.ico">
     <title>RoloLux | O nama</title>
     <link href="css/styles.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/carousel.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
+<!--    <link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
 <nav class="navbar navbar-toggleable-md navbar-light bg-danger fixed-top container">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Kontakt</a>
@@ -43,6 +42,7 @@
         </ul>
     </div>
 </nav>
+
 <div class="container">
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
@@ -57,7 +57,7 @@
                 <div class="container">
                     <div class="carousel-caption d-none d-md-block">
 
-                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p class="carousel-text">IZRADA, POPRAVKA I ODRŽAVANJE: Aluminijumskih i PVC roletni, tendi, venecijanera, trakastih zavesa...</p>
                     </div>
                 </div>
             </div>
@@ -66,7 +66,7 @@
                 <div class="container">
                     <div class="carousel-caption d-none d-md-block">
 
-                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p class="carousel-text">...rolo komarnika, Plisiranih zavesa, Harmonika vrata. Dihtovanje prozora i vrata aluminijumskim lajsnama.</p>
                     </div>
                 </div>
             </div>
@@ -75,7 +75,7 @@
                 <div class="container">
                     <div class="carousel-caption d-none d-md-block">
 
-                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p class="carousel-text">Odlična toplotna i zvučna izolacija!</p>
                     </div>
                 </div>
             </div>
@@ -84,7 +84,7 @@
                 <div class="container">
                     <div class="carousel-caption d-none d-md-block">
 
-                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p class="carousel-text">Brzo i trajno!</p>
                     </div>
                 </div>
             </div>
@@ -99,32 +99,30 @@
         </a>
     </div>
 
-
-
-
-
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 marketing">
-                <h2></h2>
-                <img src="img/logo.png" class="image_logo">
-                <p></p>
-            </div>
-            <div class="col-md-8 marketing_8">
-                <h2>O nama</h2>
-                <p>Firma Rololux ns je osnovana 1950. godine kao samostalna zanatska radnja za proizvodnju i odrzavanje roletni. Firma je pocela sa radom u Novom Sadu, gde se uspesno i odrzala vec tri generacije. Danas smo, zahvaljujući uspešnim poslovanjem postali ozbiljno privatno preduzeće, koje se bavi proizvodnjom i ugradnjom proizvoda vezanih za enterijer i eksterijer vašeg doma. Vremenom smo došli do zavidnog nivoa proizvodnje korišćenjem savremenih tehnologija, modernizacijom opreme i alata u proizvodnji i praćenjem evropskih i svetskih trendova. Sve to je dovelo do povećanja obima proizvodnje a time i povećanja kvaliteta i proširenja palete proizvoda. Mozemo vam se pohvaliti kvalitetom, strucnim i profesionalnim osobljem, raznovrsnošću programa za opremanje kuća, stanova, poslovnoih prostora i sl...Većina naših proizvoda imaju mogućnostu ugradnje elektro motora i daljinskog upravljanja. </p>
-            </div>
+    <section>
+    <div class="row">
+        <div class="col-md-4 container">
+            <h2></h2>
+            <img src="img/logo.png" class="image-logo">
+            <p></p>
+        </div>
+        <div class="col-md-8 container">
+            <h2>O nama</h2>
+            <p class="marketing-txt">Firma Rololux ns je osnovana 1950. godine kao samostalna zanatska radnja za proizvodnju i odrzavanje roletni. Firma je pocela sa radom u Novom Sadu, gde se uspesno i odrzala vec tri generacije. Danas smo, zahvaljujući uspešnim poslovanjem postali ozbiljno privatno preduzeće, koje se bavi proizvodnjom i ugradnjom proizvoda vezanih za enterijer i eksterijer vašeg doma.</p><p class="marketing-txt"> Vremenom smo došli do zavidnog nivoa proizvodnje korišćenjem savremenih tehnologija, modernizacijom opreme i alata u proizvodnji i praćenjem evropskih i svetskih trendova. Sve to je dovelo do povećanja obima proizvodnje a time i povećanja kvaliteta i proširenja palete proizvoda.</p><p class="marketing-txt"> Mozemo vam se pohvaliti kvalitetom, strucnim i profesionalnim osobljem, raznovrsnošću programa za opremanje kuća, stanova, poslovnoih prostora i sl...Većina naših proizvoda imaju mogućnostu ugradnje elektro motora i daljinskog upravljanja. </p>
         </div>
     </div>
-    <footer class="footy bg-danger">
-        <p class="float-right"><a href="#" class="back">Back to top</a></p>
-        <p>&copy; 2017</p><img src="img/logo.png" class="logo-img img-fluid">
-    </footer>
+    </section>
+
+    <section>
+        <footer class="footy bg-danger">
+            <p class="float-right"><a href="#" class="back">Back to top</a></p>
+            <img src="img/logo.png" class="logo-img img-fluid">
+            <p>&nbsp;&nbsp;&copy; 2017 &nbsp;&nbsp;<a href="https://www.facebook.com/RololuxNs"><i class="fa fa-facebook-official big-icon"></i></a></p>
+        </footer>
+    </section>
 </div>
 </div>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script src="js/carousel.js"></script>
 </body>
 </html>
