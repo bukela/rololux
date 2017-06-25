5273e5ff91f17f582b8f266585436731b49325e1
bukela <bukela@gmail.com>
2017-06-25 22:34:30 +0200

    init devel

---
+++ README.md
@@ -0,0 +1,41 @@
+
+
+Tel.: 021/ 2466 467
+Mob.: 063/ 8 063 531 i 064/ 115 95 32
+
+    Početna
+    O nama
+    Kontakt
+
+Naša ponuda
+
+    Roletne
+    Tende
+    Venecijaneri
+    Zavese
+    Komarnici
+    Dihtovanje
+    Servis
+    Adaptacija prostora
+
+    IZRADA, POPRAVKA I ODRŽAVANJE: Aluminijumskih i PVC roletni, tendi, venecijanera, trakastih zavesa..
+    Rolo komarnika, Plisiranih zavesa, Harmonika vrata, Dihtovanje prozora i vrata aluminijumskim lajsnama
+    Odlična toplotna i zvučna izolacija!
+    Brzo i trajno!
+
+
+    Početna | O nama | Kontakt 
+
+
+
+Subotička 67
+21 000 Novi Sad, Srbija
+home-icon
+
+fax 021/ 2466-467
+mob 063/ 8-063-531
+mob 064/ 11-59-532
+home-icon
+
+rololuxns@yahoo.com
+RoloLux
---
+++ webroot/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.ico">
+    <title>RoloLux | O nama</title>
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
+</head>
+
+<body>
+<nav class="navbar navbar-toggleable-md navbar-light bg-danger fixed-top container">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kontakt</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Naša ponuda</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item my-link" href="roletne.html">Roletne</a>
+                    <a class="dropdown-item my-link" href="tende.html">Tende</a>
+                    <a class="dropdown-item my-link" href="venecijaneri.html">Venecijaneri</a>
+                    <a class="dropdown-item my-link" href="rolo_zavese.html">Rolo zavese</a>
+                    <a class="dropdown-item my-link" href="trakaste_zavese.html">Trakaste zavese</a>
+                    <a class="dropdown-item my-link" href="komarnici.html">Komarnici</a>
+                    <a class="dropdown-item my-link" href="dihtovanje.html">Dihtovanje</a>
+                    <a class="dropdown-item my-link" href="servis.html">Servis</a>
+                    <a class="dropdown-item my-link" href="adaptacija_prostora.html">Adaptacija prostora</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img class="first-slide" src="img/slider/home/pic1.jpg" alt="First slide">
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+
+                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="second-slide" src="img/slider/home/pic2.jpg" alt="Second slide">
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+
+                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="third-slide" src="img/slider/home/pic3.jpg" alt="Third slide">
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+
+                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="third-slide" src="img/slider/home/pic4.jpg" alt="Third slide">
+                <div class="container">
+                    <div class="carousel-caption d-none d-md-block">
+
+                        <p class="carousel-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+
+
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 marketing">
+                <h2></h2>
+                <img src="img/logo.png" class="image_logo">
+                <p></p>
+            </div>
+            <div class="col-md-8 marketing_8">
+                <h2>O nama</h2>
+                <p>Firma Rololux ns je osnovana 1950. godine kao samostalna zanatska radnja za proizvodnju i odrzavanje roletni. Firma je pocela sa radom u Novom Sadu, gde se uspesno i odrzala vec tri generacije. Danas smo, zahvaljujući uspešnim poslovanjem postali ozbiljno privatno preduzeće, koje se bavi proizvodnjom i ugradnjom proizvoda vezanih za enterijer i eksterijer vašeg doma. Vremenom smo došli do zavidnog nivoa proizvodnje korišćenjem savremenih tehnologija, modernizacijom opreme i alata u proizvodnji i praćenjem evropskih i svetskih trendova. Sve to je dovelo do povećanja obima proizvodnje a time i povećanja kvaliteta i proširenja palete proizvoda. Mozemo vam se pohvaliti kvalitetom, strucnim i profesionalnim osobljem, raznovrsnošću programa za opremanje kuća, stanova, poslovnoih prostora i sl...Većina naših proizvoda imaju mogućnostu ugradnje elektro motora i daljinskog upravljanja. </p>
+            </div>
+        </div>
+    </div>
+    <footer class="footy bg-danger">
+        <p class="float-right"><a href="#" class="back">Back to top</a></p>
+        <p>&copy; 2017</p><img src="img/logo.png" class="logo-img img-fluid">
+    </footer>
+</div>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/carousel.js"></script>
+</body>
+</html>
