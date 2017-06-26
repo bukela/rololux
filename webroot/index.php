f51741efd95c3827e100e565ac22b953bbc7ae0f
bukela <bukela@gmail.com>
2017-06-27 01:14:51 +0200

    centering

--- webroot/index.php
+++ webroot/index.php
@@ -6,16 +6,16 @@
     <link rel="icon" href="img/favicon.ico">
     <title>RoloLux | O nama</title>
     <link href="css/style.css" rel="stylesheet">
-<!--    <link href="css/font-awesome.min.css" rel="stylesheet">-->
+    <!--    <link href="css/font-awesome.min.css" rel="stylesheet">-->
     <link href="css/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
 <nav class="navbar navbar-toggleable-md navbar-light bg-danger fixed-top container">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+    </button>
     <a class="navbar-brand" href="index.php"><img src="img/logo.png" class="img-fluid"></a>
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
@@ -98,33 +98,32 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
-    </div>
-
-    <section>
+</div>
+<section>
     <div class="container">
-    <div class="row">
-        <div class="col-md-4 container">
-            <h2></h2>
-            <img src="img/logo.png" class="image-logo">
-            <p></p>
+        <div class="row">
+            <div class="col-md-4 container">
+                <h2></h2>
+                <img src="img/logo.png" class="image-logo">
+                <p></p>
+            </div>
+            <div class="col-md-8 container">
+                <h2>O nama</h2>
+                <p class="marketing-txt">Firma Rololux ns je osnovana 1950. godine kao samostalna zanatska radnja za proizvodnju i odrzavanje roletni. Firma je pocela sa radom u Novom Sadu, gde se uspesno i odrzala vec tri generacije. Danas smo, zahvaljujući uspešnim poslovanjem postali ozbiljno privatno preduzeće, koje se bavi proizvodnjom i ugradnjom proizvoda vezanih za enterijer i eksterijer vašeg doma.</p><p class="marketing-txt"> Vremenom smo došli do zavidnog nivoa proizvodnje korišćenjem savremenih tehnologija, modernizacijom opreme i alata u proizvodnji i praćenjem evropskih i svetskih trendova. Sve to je dovelo do povećanja obima proizvodnje a time i povećanja kvaliteta i proširenja palete proizvoda.</p><p class="marketing-txt"> Mozemo vam se pohvaliti kvalitetom, strucnim i profesionalnim osobljem, raznovrsnošću programa za opremanje kuća, stanova, poslovnoih prostora i sl...Većina naših proizvoda imaju mogućnostu ugradnje elektro motora i daljinskog upravljanja. </p>
+            </div>
+
+
         </div>
-        <div class="col-md-8 container">
-            <h2>O nama</h2>
-            <p class="marketing-txt">Firma Rololux ns je osnovana 1950. godine kao samostalna zanatska radnja za proizvodnju i odrzavanje roletni. Firma je pocela sa radom u Novom Sadu, gde se uspesno i odrzala vec tri generacije. Danas smo, zahvaljujući uspešnim poslovanjem postali ozbiljno privatno preduzeće, koje se bavi proizvodnjom i ugradnjom proizvoda vezanih za enterijer i eksterijer vašeg doma.</p><p class="marketing-txt"> Vremenom smo došli do zavidnog nivoa proizvodnje korišćenjem savremenih tehnologija, modernizacijom opreme i alata u proizvodnji i praćenjem evropskih i svetskih trendova. Sve to je dovelo do povećanja obima proizvodnje a time i povećanja kvaliteta i proširenja palete proizvoda.</p><p class="marketing-txt"> Mozemo vam se pohvaliti kvalitetom, strucnim i profesionalnim osobljem, raznovrsnošću programa za opremanje kuća, stanova, poslovnoih prostora i sl...Većina naših proizvoda imaju mogućnostu ugradnje elektro motora i daljinskog upravljanja. </p>
-        </div>
-    
-    
     </div>
-    </div>
-    </section>
-    <section>
-       <div class="container">
+</section>
+<section>
+    <div class="container">
         <footer class="footy bg-danger">
             <p class="float-right"><a href="#" class="back">Back to top</a></p>
             <p class="footy-txt">&nbsp;&nbsp;RoloLux NS&nbsp;&nbsp;&copy; 2017 &nbsp;&nbsp;<a href="https://www.facebook.com/RololuxNs"><i class="fa fa-facebook-official big-icon"></i></a></p>
         </footer>
-        </div>
-    </section>
+    </div>
+</section>
 
 
 <script src="js/jquery.min.js"></script>
